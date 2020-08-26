c6d99f835483bd8173bed81a6b667bb4fa1b95b1
John Audia <graysky@archlinux.us>
2020-08-26 06:29:39 -0400

    kernel: bump 5.4 to 5.4.75
    Manually rebased patches:
      bcm27xx:
        patches-5.4/950-0267-xhci-add-quirk-for-host-controllers-that-don-t-updat.patch
      bcm53xx:
        patches-5.4/180-usb-xhci-add-support-for-performing-fake-doorbell.patch
      layerscape:
        patches-5.4/802-can-0025-can-flexcan-add-LPSR-mode-support-for-i.MX7D.patch
        patches-5.4/808-i2c-0002-MLK-10893-i2c-imx-add-irqf_no_suspend.patch
        patches-5.4/820-usb-0016-MLK-16735-usb-host-add-XHCI_CDNS_HOST-flag.patch
    
    Removed since could be reverse-applied by quilt:
      mediatek:
        patches-5.4/0700-arm-dts-mt7623-add-missing-pause-for-switchport.patch
    
    All modifications made by update_kernel.sh
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, ath79/generic, bcm27xx/bcm2711, x86_64
    Run-tested: ipq806x/R7800, x86_64
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>
    Tested-by: Curtis Deptuck <curtdept@me.com> [x86_64]
    
    Rebase of 802-can-0025-can-flexcan-add-LPSR-mode-support-for-i.MX7D.patch
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>
    Signed-off-by: Kevin Darbyshire-Bryant <kevin@darbyshire-bryant.me.uk>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -9,12 +9,12 @@ endif
 LINUX_VERSION-4.9 = .243
 LINUX_VERSION-4.14 = .209
 LINUX_VERSION-4.19 = .148
-LINUX_VERSION-5.4 = .60
+LINUX_VERSION-5.4 = .75
 
 LINUX_KERNEL_HASH-4.9.243 = d3aa189ca7fcc6e52d6c0333a0d7acd8789e9a492b32dbf9476e926ffaa73984
 LINUX_KERNEL_HASH-4.14.209 = cb02465cc8f1972cc14707b25d779c8668d220c39e68a24bb23afd4c58182b9c
 LINUX_KERNEL_HASH-4.19.148 = 3308c74a93ce6d4e495fde35d1351061240a895f76a288248c343713bd63555b
-LINUX_KERNEL_HASH-5.4.60 = add2ab2385c40fc9a3dfebe403e56da8500b633dc7dc42cf0c670c61d151a223
+LINUX_KERNEL_HASH-5.4.75 = d2466fd6eb5433e7bf287b617b11b2640c65a7ea93a57eb7a80d7f537cbc1470
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
